aa5a5271680fab1309ea643335be9e4525008ae7
chinmoybuzz <chinmoybuzz@gmail.com>
2026-02-21 20:24:45 +0530

    testing route dotenv

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const githubRoutes = require("./routes/github");
-
+const testRoutes = require("./routes/test.route");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/auth/github", githubRoutes);
-
+app.use("/ping", testRoutes);
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
